a3a088e428dbfc9cc09630149b5b477dc7ca5450
elrrrrrrr <elrrrrrrr@gmail.com>
2014-05-28 17:02:39 +0800

    update

---
+++ js/data.js
@@ -0,0 +1,11 @@
+// pageTracker._trackPageview("events/market/ttzj/hangzhou");
+
+window.GROUP_ID = 192;
+
+window.storeList = [{
+    "name": "MeliaTokyo魅力东京造型",
+    "img": "img/1.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"
+}];
