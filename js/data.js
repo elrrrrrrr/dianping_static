a8eaa64ccc1550a0d94bf1efeedc330caef4b35f
elrrrrrrr <elrrrrrrr@gmail.com>
2014-05-29 12:09:43 +0800

    save

---
+++ js/data.js
@@ -0,0 +1,38 @@
+// pageTracker._trackPageview("events/market/ttzj/hangzhou");
+
+
+//0-美甲 1-美发 2-遇见 3-密室
+window.type='3';
+window.storeList = [
+    {"name": "MeliaTokyo魅力东京造型",
+    "img": "img/1.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"},
+    {"name": "香格俪莱(时代店)",
+    "img": "img/2.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"},
+    {"name": "小左美发(武宁路店)",
+    "img": "img/3.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"},
+    {"name": "MeliaTokyo魅力东京造型",
+    "img": "img/4.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"},
+    {"name": "xy hair salon(人民广场店）",
+    "img": "img/5.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"},
+    {"name": "MeliaTokyo魅力东京造型",
+    "img": "img/6.jpg",
+    "promotion": "http://www.dianping.com/promo/214182?source=1",
+    "coupon": "http://t.dianping.com/deal/2171608",
+    "link": "http://www.dianping.com/shop/6057548"},
+
+];
