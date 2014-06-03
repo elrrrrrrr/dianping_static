1ee461ad6c23f78b6e9948a9ed74e703c5b94fa6
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-03 22:48:34 +0800

    add jsyh

---
+++ case/jsyh/shop.js
@@ -0,0 +1,52 @@
+[{
+	"type" : "food",
+	"data" : [{
+		"image" : "json/img/lx/logo.jpg",
+		"title" : "龙虾宝宝",
+		"href" : "http://www.dianping.com/shop/18241286",
+		"discrible" : "麻辣龙虾宝宝，属北京紫翔厨煌餐饮管理有限公司旗下品牌店之一、现天津、广州...",
+		"more" : "麻辣龙虾宝宝，属北京紫翔厨煌餐饮管理有限公司旗下品牌店之一、现天津、广州、上海均有分店。线下体验店座落在京城繁华北京市中心金宝街6号。龙虾宝宝主张“以时尚创新为元素、质量保证为根基，打造品味、方便、时尚、快捷的特色美食”，坚持“以白领时尚年轻人的需求为导向”、迄今龙虾宝宝已拥有十万多人的忠实客户，在专营特色美食行业脱颖而出，为麻小爱好者提供了很大的便利。",
+		"scrollimage" : ["json/img/lx/1.jpg", "json/img/lx/2.jpg", "json/img/lx/3.jpg", "json/img/lx/4.jpg", "json/img/lx/5.jpg", "json/img/lx/6.jpg", "json/img/lx/7.jpg", "json/img/lx/8.jpg"]
+	}, {
+		"image" : "json/img/y16/logo.jpg",
+		"title" : "乙十六",
+		"href" : "http://www.dianping.com/shop/4704484",
+		"discrible" : "乙十六体育嘉年华 -世界级体育盛世2014巴西世界杯又将如期而至。在这样一个全球...",
+		"more" : "乙十六体育嘉年华 -世界级体育盛世2014巴西世界杯又将如期而至。在这样一个全球同欢同乐的盛大赛事，你还窝在家中的沙发上看电视球赛吗？在这个夏天，“乙十六体育嘉年华”已经为你准备好了一切——提供最高品质的观球场地，打造最激情豪放的观球氛围。与世界杯同步连线直播，球迷评球第二赛事外景现场。竞猜球队胜负比分，亲手抽取球星签名大奖。吃巴西自助烤肉，喝德国慕尼黑啤酒。这个夏天还有比这更爽的事吗？！",
+		"yhq" : "http://www.dianping.com/promo/223279?source=1",
+		"scrollimage" : ["json/img/y16/1.jpg", "json/img/y16/2.jpg", "json/img/y16/3.jpg", "json/img/y16/4.jpg", "json/img/y16/5.jpg", "json/img/y16/2.jpg", "json/img/y16/3.jpg", "json/img/y16/4.jpg"]
+	}, {
+		"image" : "json/img/jll/logo.JPG",
+		"title" : "嘉陵楼馋嘴城",
+		"href" : "http://www.dianping.com/search/keyword/2/0_%E5%98%89%E9%99%B5%E6%A5%BC%E9%A6%8B%E5%98%B4%E5%9F%8E",
+		"discrible" : "嘉陵楼馋嘴城：原创馋嘴蛙 馋嘴菜的发源地始创于1995年京城东直门簋街一家仅60平方米...",
+		"more" : "嘉陵楼馋嘴城：原创馋嘴蛙 馋嘴菜的发源地始创于1995年京城东直门簋街一家仅60平方米、摆放了几张小桌的“同福居涮鲜肉馆”，它是嘉陵楼馋嘴城的前身，也是金簋小山城餐饮集团的的前身。也是在这研发的馋嘴蛙得到八方食圣金牌菜的开始并发扬。现在，它还是作为集团菜品研发的先锋，正在把馋嘴系列放大，把原创的馋嘴料应用到更多的食材和菜品中！更好的发展馋嘴菜系！让更多的馋嘴爱好者能够享受到馋嘴系列美食。",
+		"tg" : "http://t.dianping.com/deal/6141260",
+		"yhq" : "http://www.dianping.com/promo/223324",
+		"yd" : "http://www.dianping.com/shop/5453947#reserve",
+		"scrollimage" : ["json/img/jll/1.jpg", "json/img/jll/2.jpg", "json/img/jll/3.jpg", "json/img/jll/4.jpg", "json/img/jll/1.jpg", "json/img/jll/2.jpg", "json/img/jll/3.jpg", "json/img/jll/4.jpg"]
+	}, {
+		"image" : "json/img/mm/logo.png",
+		"title" : "没名儿生煎",
+		"href" : "http://www.dianping.com/search/keyword/2/0_%E6%B2%A1%E5%90%8D%E5%84%BF%E7%94%9F%E7%85%8E",
+		"discrible" : "没名儿生煎创建于2011年7月，以生煎包为招牌特色，主要经营各类中华小吃，以其精益求精...",
+		"more" : "没名儿生煎创建于2011年7月，以生煎包为招牌特色，主要经营各类中华小吃，以其精益求精的饮食文化、体贴细腻的人性化服务，倾力打造中国餐饮一线品牌。",
+		"scrollimage" : ["json/img/mm/1.JPG", "json/img/mm/2.JPG", "json/img/mm/3.jpg", "json/img/mm/4.png", "json/img/mm/5.JPG", "json/img/mm/6.JPG", "json/img/mm/3.jpg", "json/img/mm/4.png"]
+	}, {
+		"image" : "json/img/kbd/logo.jpg",
+		"title" : "宽板凳",
+		"href" : "http://www.dianping.com/search/keyword/2/0_%E5%AE%BD%E6%9D%BF%E5%87%B3%E8%80%81%E7%81%B6%E7%81%AB%E9%94%85",
+		"discrible" : "宽板凳老灶火锅以宣扬重庆100多年历史的最正宗的老灶火锅为初衷，把重庆独具魅力...",
+		"more" : "宽板凳老灶火锅以宣扬重庆100多年历史的最正宗的老灶火锅为初衷，把重庆独具魅力的、原滋原味的、脍炙人口的饮食文化带给历史文化古都及来自五湖四海的朋友们！“宽板凳”一直以“弘扬和传承重庆老火锅文化”为使命，无论是店面的装修风格、餐具还是味道，都保持着重庆老火锅的风格。在重庆火锅特色食材上的选用也是精心由重庆挑选后空运上北京的，如宽板凳鲜毛肚、宽板凳鲜鹅肠等。",
+		"tg" : "http://t.dianping.com/deal/2308843",
+		"scrollimage" : ["json/img/kbd/1.jpg", "json/img/kbd/2.jpg", "json/img/kbd/3.jpg", "json/img/kbd/4.jpg", "json/img/kbd/1.jpg", "json/img/kbd/2.jpg", "json/img/kbd/3.jpg", "json/img/kbd/4.jpg"]
+	}, {
+		"image" : "json/img/xd/logo.bmp",
+		"title" : "兄弟川菜",
+		"href" : "http://www.dianping.com/search/keyword/2/0_%E5%85%84%E5%BC%9F%E5%B7%9D%E8%8F%9C",
+		"discrible" : "兄弟川菜：品质川菜，正宗食飨15年来，兄弟川菜品质如一，深受广大食客喜爱。泉水...",
+		"more" : "兄弟川菜：品质川菜，正宗食飨15年来，兄弟川菜品质如一，深受广大食客喜爱。泉水牛蛙、毛血旺、麻辣龙虾、口水鸡等等总是让人赞不绝口。环境时尚又不失中国风，非常适合朋友家人聚餐、情侣约会。兄弟川菜旗舰店（又名川上四季）又是一个新亮点，一二楼是一样的川菜，一样的优美环境，三楼有新开业露天花园大排档。有没有心动呢？心动不如行动，想吃正宗请到兄弟川菜！经营宗旨：做更好吃的川菜，提供更体贴的服务！",
+		"hyk" : "http://www.dianping.com/card/detail/c3_b4562848_a22#4562848",
+		"scrollimage" : ["json/img/xd/1.jpg", "json/img/xd/2.jpg", "json/img/xd/3.jpg", "json/img/xd/4.jpg","json/img/xd/1.jpg", "json/img/xd/2.jpg", "json/img/xd/3.jpg", "json/img/xd/4.jpg"]
+	}]
+}]
