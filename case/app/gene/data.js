89eab5e043173b01c3704bc421c375c72d55fe83
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-09 22:31:04 +0800

    fix the gene app template app link

--- case/app/gene/data.js
+++ case/app/gene/data.js
@@ -2,43 +2,67 @@
 
 
 //0-美甲 1-美发 2-遇见 3-密室
-window.type = '0';
+window.type = '3';
+window.banner='img/banner.jpg';
 window.storeList = [{
-        "name": "La Vida 乐薇美甲美睫美妆沙龙(海防路店)",
-        "imgs": ["img/0/1.jpg"],
+        "name": "Pulupulu主题游戏馆",
+        "imgs": ["img/3/1-1.jpg","img/3/1-2.jpg"],
         "promotion": "",
-        "coupon": "",
-        "link": "http://www.dianping.com/shop/12596920"
+        "coupon": "dianping://tuandeal?id=2031990",
+        "link": "dianping://shopinfo?id=13813428"
     }, {
-        "name": "恋色美容美甲美睫SPA会所",
-        "imgs": ["img/0/2.jpg"],
+        "name": "X先生密室(黄浦旗舰店)",
+        "imgs": ["img/3/2.jpg"],
+        "promotion": "http://www.dianping.com/promo/221204?source=1",
+        "coupon": "dianping://tuandeal?id=166787",
+        "link": "dianping://shopinfo?id=6028133"
+    }, {
+        "name": "Black Queen黑桃皇后密室逃脱",
+        "imgs": ["img/3/3.jpg"],
         "promotion": "",
-        "coupon": "",
-        "link": "http://www.dianping.com/shop/18274863"
-    }, {
-        "name": "Sweety Baby美睫美甲(茂名北路店)",
-        "imgs": ["img/0/3.jpg"],
-        "promotion": "http://www.dianping.com/promo/222067?source=1",
-        "coupon": "http://t.dianping.com/deal/2287632",
-        "link": "http://www.dianping.com/shop/15966096"
+        "coupon": "dianping://tuandeal?id=2082681",
+        "link": "dianping://shopinfo?id=15859249"
     },
     {
-        "name": "Beloved Nail呗珑专业美甲沙龙三千丝",
-        "imgs": ["img/0/4.jpg"],
+        "name": "FBI真人密室",
+        "imgs": ["img/3/4.jpg"],
         "promotion": "",
-        "coupon": "http://t.dianping.com/deal/2124999",
-        "link": "http://www.dianping.com/shop/15999620"
+        "coupon": "",
+        "link": "dianping://shopinfo?id=18217327"
     }, {
-        "name": "Miss Queen美睫屋(香黛广场店)",
-        "imgs": ["img/0/5.jpg"],
-        "promotion": "http://www.dianping.com/promo/221890?source=1",
-        "coupon": "http://t.dianping.com/deal/2217415",
-        "link": "http://www.dianping.com/shop/17705032"
-    }, {
-        "name": "A&D Nail Salon",
-        "imgs": ["img/0/6.jpg"],
+        "name": "极道真人密室逃脱(人民广场大世界店)",
+        "imgs": ["img/3/5.jpg"],
         "promotion": "",
-        "coupon": "http://t.dianping.com/deal/2227057",
-        "link": "http://www.dianping.com/shop/17178271"
-    },
+        "coupon": "",
+        "link": "dianping://shopinfo?id=18179096"
+    }, {
+        "name": "睿趣Reach真人密室逃脱(四川北路店)",
+        "imgs": ["img/3/6.jpg"],
+        "promotion": "",
+        "coupon": "dianping://tuandeal?id=2049465",
+        "link": "dianping://shopinfo?id=8854466"
+    }
 ];
+
+( function  () {
+    var os, promoid,
+        platform = navigator.platform,
+        ua = navigator.userAgent,
+        map = {
+            'android' : 'dianping://newpromoinfo?type=2&promoid=',
+            'ios': 'dianping://promoinfo?promoid='
+        };  
+
+    if (platform.match(/iPhone|iPod|iPad/i)) {
+        os = "ios";
+    } else if (platform.match(/Linux/i) && ua.match(/android|Silk/i)) {
+        os = "android";
+    }
+
+    _.each(window.storeList, function (item) {
+        promoid = item.promotion.match(/(\d+)\?/);
+        if (promoid) {
+            item.promotion = map[os] +  promoid[1] + (os == 'ios' ? '&shopid=' + item.link.match(/\?id=(\d+)/)[1] : '');
+        }
+    });
+})();
