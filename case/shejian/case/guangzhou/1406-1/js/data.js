763669f85ba8c9025cab67984818f8bbde08641a
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-09 22:29:50 +0800

    add shejian case

---
+++ case/shejian/case/guangzhou/1406-1/js/data.js
@@ -0,0 +1,108 @@
+// pageTracker._trackPageview("events/market/ttzj/hangzhou");
+
+/*
+
+		"name": "",
+        "imgs": ["img/4.jpg"],
+        "introduction":"",
+        "reservation":"",
+        "promotion": "",
+        "coupon": "",
+        "link": "",
+        "member":""
+*/
+
+//0-美甲 1-美发 2-遇见 3-密室 4-舌尖
+window.type = '4';
+window.storeList = [{
+        "name": "喜锅潮汕牛肉馆",
+        "imgs": ["img/1.jpg"],
+        "reservation":"",
+        "introduction":"“山不在高，有仙则名；水不在深，有龙则灵。”浓香潮牛火锅店，只做潮汕本土黄牛肉，且特别精选身材苗条的瘦黄牛，只因其肥而不腻，瘦而不柴，肉质更有牛味，更富韧性。其味鲜，香，嫩，韧，让人流连忘返。",
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/2113546",
+        "link": "http://www.dianping.com/shop/5511706",
+        "member":"http://www.dianping.com/card/detail/c3_b5511706_a20"
+    }, {
+        "name": "西游记欢乐餐厅",
+        "imgs": ["img/2.jpg"],
+        "introduction":"健康中国风，欢乐西游记---西游记欢乐餐厅，广州十大主题餐厅，招牌牛魔王牛腩，广州最有牛味的牛腩，荣获广东地标美食大奖，广州首创椰汁芒果糯米饭，更令人回味无穷。十年感恩用心，不断推陈出新，13个系列，过百款甜品，美食，感动崇尚健康喜欢美食的您！",
+        "reservation":"",
+        "promotion": "",
+        "coupon": "",
+        "link": "http://www.dianping.com/shop/5323088",
+        "member":""
+    }, {
+        "name": "和汉食府",
+        "imgs": ["img/3.jpg"],
+        "introduction":"“和汉食府”粤菜老店，厨师也深谙粤菜精华，让每一道寻常菜品都刻上了正宗的广州风味。出品精致，地道风味，以选料精细，操作考究，特色鲜明而享有盛誉。",
+        "reservation":"",
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/6109396",
+        "link": "http://www.dianping.com/shop/17702566",
+        "member":""
+    }, {
+        "name": "巫山碳烤鱼",
+        "imgs": ["img/4.jpg"],
+        "introduction":"巫山碳烤鱼位于广州市热门商圈——天河城体育中心时尚天河城香榭丽舍美食区，烤鱼有许多口味可选，味道“没得说”，皮可以“大块”地撕下来吃，那叫一个“过瘾”，肉也烤得“恰到好处”，整个一“外焦里嫩”，配菜的话，选土豆、莴笋、豆皮、木耳等“比较经典”。还有“免费”瓜子和茶水“无限供应”，真是“聊天乱侃”的好地方。",
+        "reservation":"",
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/5047519",
+        "link": "http://www.dianping.com/shop/13904522",
+        "member":""
+    }, {
+    	"name": "膳通汕头牛肉馆",
+        "imgs": ["img/5.jpg"],
+        "introduction":"膳通汕头牛肉馆，正宗黄牛肉，澄海小吃，出品浓缩牛肉之精华，潮汕牛肉火锅之精髓！每天新鲜的货源，刀功熟练的切肉师傅，提供优雅、高档、时尚、有品位的就餐环境和服务水准，保证您体验到最正宗潮汕牛肉！",
+        "reservation":"",
+        "promotion": "http://www.dianping.com/promo/223731?source=1",
+        "coupon": "",
+        "link": "http://www.dianping.com/shop/18145054",
+        "member":""
+    },{
+        "name": "香香妈妈砂窝湘菜",
+        "imgs": ["img/6.jpg"],
+        "introduction":"香香妈妈砂锅湘菜，是一家配以地道湘味小炒，凉菜和点心的中式餐厅。致力于打造湘菜新典范；明亮、宽敞，幽雅、独特的就餐环境，在这里用餐会让人心情愉悦，倍显尊贵。剁椒蒸鱼头、砂锅手撕鸡，菊花酱汁肉等是咱家的特色。平民化消费，优质的星级享受，是朋友及家庭聚餐的不二选择。",
+        "reservation":"",
+        "promotion": "http://www.dianping.com/promo/223412?source=1",
+        "coupon": "http://t.dianping.com/deal/2297459",
+        "link": "http://www.dianping.com/shop/6339575",
+        "member":""
+    },{
+    	"name": "皇贡土菜",
+        "imgs": ["img/7.jpg"],
+        "introduction":" 皇贡土菜，为“土菜名师”单先生所创办，聘请衡东土菜名厨，选用自然放养的肉禽以及自然野生种植的绿色食材，以传统的方法烹饪出具有独特地方美味的佳肴，既保留衡东土菜鲜、美、辣的淳朴风格，又融入粤菜文化精、巧、艳的时尚韵味，为大都市的人们带来简约快乐的生活体验，为“吃在广州”增添清新色彩。",
+        "reservation":"",
+        "promotion": "http://www.dianping.com/promo/221990?source=1",
+        "coupon": "http://t.dianping.com/deal/2227611",
+        "link": "http://www.dianping.com/shop/17672953",
+        "member":""
+    },{
+        "name": "瑞东阁安徽私房菜",
+        "imgs": ["img/8.jpg"],
+        "introduction":"也许我没有华丽的外衣，也许我不曾冠以引人侧目的名号，然食之本色不正当如此吗？前行的道路其漫漫修远，未尝独行，只因有你；常怀感恩，只因信任。因为有你，我才能更好！",
+        "reservation":"",
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/5129757",
+        "link": "http://www.dianping.com/shop/6008059",
+        "member":""
+    },{
+        "name": "香港四季港茶餐厅",
+        "imgs": ["img/9.jpg"],
+        "introduction":"香港四季港茶餐厅是一家以港式烧味和粤菜小炒为特色的港式饮食名店，餐厅菜式种类繁多，卖相精致，环境格调高雅。珠江新城高德置地分店，最大的特点莫过于由世界知名设计师Bosco Lam打造的私家蒲场特色装修，他把当地的餐饮文化结合到现代前卫的艺术设计中。从餐桌、座椅到挂饰乃至吊灯每一个细节，全都是现代艺术设计的风格。​",
+        "reservation":"",
+        "promotion": "http://www.dianping.com/promo/222678?source=1",
+        "coupon": "http://t.dianping.com/deal/6033127",
+        "link": "http://www.dianping.com/shop/17824339",
+        "member":""
+    },{
+        "name": "小飞生煎",
+        "imgs": ["img/10.jpg"],
+        "introduction":"小飞生煎位于体育西路后街（地铁体育西站A出口即到），主打上海名点“生煎包”，出品正宗，风味独特，小飞生煎倡导绿色健康饮食，选料上乘，采用半开放式的厨房，让消费者明明白白，放心消费。吃正宗好生煎三大步骤：开口、喝汤、吃肉。小飞生煎诚挚欢迎广大顾客前来品尝。",
+        "reservation":"",
+        "promotion": "",
+        "coupon": "http://t.dianping.com/deal/2231729",
+        "link": "http://www.dianping.com/shop/8966420",
+        "member":""
+    }
+];
